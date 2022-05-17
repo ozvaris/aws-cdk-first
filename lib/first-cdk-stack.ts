aa4738ea4c65ee1298b24678fe16f508e4f75c43
Ozgur <ozgurozvaris@hotmail.com>
2022-05-17 10:10:52 +0100

    initial commit

--- lib/first-cdk-stack.ts
+++ lib/first-cdk-stack.ts
@@ -1,16 +1,18 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Bucket } from "aws-cdk-lib/aws-s3";
 
 export class FirstCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'FirstCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new Bucket(this, "someBucket", {
+      lifecycleRules: [
+        {
+          expiration: Duration.days(2),
+        },
+      ],
+    });
   }
 }
